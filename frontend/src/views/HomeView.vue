ed3bc68a8f3aaee40a358228771f998d7de73053
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-04-24 14:02:20 +0200

    Added BentoGrid to HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,66 @@
 <script setup>
 import {Button} from "@/components/ui/button/index.js";
+import { BentoCard, BentoGrid } from '@/components/ui/bento'
 </script>
 
 <template>
   <div class="m-auto">
-    <p>Home</p>
-    <Button>Lol</Button>
-  </div>
-  <div>
+    <BentoGrid>
+      <!-- Card 1 -->
+      <BentoCard
+        name="Krisescenarioer"
+        description="Les om forskjellige krisescenarioer"
+        cta="Les mer"
+        href="/krise"
+        :Icon="ArrowRightIcon"
+        customClass="col-span-2"
+      >
+        <template #background>
+          <div class="absolute inset-0 bg-neutral-200" />
+        </template>
+      </BentoCard>
+
+      <!-- Card 2 -->
+      <BentoCard
+        name="Krisehåndtering"
+        description="Generell info om krisehåndtering (før/under/etter)"
+        cta="Les mer"
+        href="/info"
+        :Icon="ArrowRightIcon"
+        customClass="col-span-2"
+      >
+        <template #background>
+          <div class="absolute inset-0 bg-neutral-200" />
+        </template>
+      </BentoCard>
+
+      <!-- Nyheter -->
+      <BentoCard
+        name="Nyheter"
+        cta="Gå til nyheter"
+        href="/nyheter"
+        :Icon="ArrowRightIcon"
+        customClass="col-span-1 min-h-[20rem]"
+      >
+        <template #background>
+          <div class="absolute inset-0 bg-neutral-200" />
+        </template>
+      </BentoCard>
+
+      <!-- Utgår snart -->
+      <BentoCard
+        name="Utgår snart"
+        description="Du er innlogget og vil se hva som snart går ut på dato"
+        cta="Se detaljer"
+        href="/utgar"
+        :Icon="ArrowRightIcon"
+        customClass="col-span-3 min-h-[24rem]"
+      >
+        <template #background>
+          <div class="absolute inset-0 bg-neutral-200" />
+        </template>
+      </BentoCard>
+    </BentoGrid>
   </div>
 
 </template>
