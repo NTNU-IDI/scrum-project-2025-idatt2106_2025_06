05f078a16d548a9d1296681971c7a1cdc488bfcb
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-04-25 09:16:52 +0200

    Fixed scrolling in news area

--- frontend/src/components/ui/bento/BentoCardCustom.vue
+++ frontend/src/components/ui/bento/BentoCardCustom.vue
@@ -4,7 +4,6 @@
       'relative col-span-1 flex flex-col justify-between overflow-hidden rounded-xl',
       'bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
       'transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]',
-      'pointer-events-none',  // Forhindre hover-interaksjoner
       customClass
     ]"
   >
@@ -14,7 +13,7 @@
     </div>
 
     <!-- Slot for custom content (som alerts eller annet innhold), plassert under tittelen -->
-    <div class="relative mt-12 p-4 z-0"> <!-- mt-12 for å legge plass til tittelen -->
+    <div class="relative mt-10 p-4 z-0"> <!-- mt-12 for å legge plass til tittelen -->
       <slot></slot>
     </div>
   </div>
