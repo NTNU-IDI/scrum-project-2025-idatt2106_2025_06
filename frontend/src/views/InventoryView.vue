3a86b261e949a25d70b87fda117d8b09b1c8eaa1
Carine Margrethe Rondeel <carinemr@stud.ntnu.no>
2025-04-25 13:01:09 +0200

    Implement basis for inventory with test data

--- frontend/src/views/InventoryView.vue
+++ frontend/src/views/InventoryView.vue
@@ -1,8 +1,36 @@
-<script setup></script>
+<script setup>
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import InventoryTable from '@/components/InventoryTable.vue'
+</script>
 
 <template>
   <div class="m-auto">
     <p>Lager</p>
+
+    <Tabs default-value="matOgDrikke" class="w-full max-w-5xl">
+      <TabsList>
+        <TabsTrigger value="matOgDrikke">Mat og drikke</TabsTrigger>
+        <TabsTrigger value="varmeOgLys">Varme og lys</TabsTrigger>
+        <TabsTrigger value="informasjon">Informasjon</TabsTrigger>
+        <TabsTrigger value="legemidOgHygiene">Legemidler og hygiene</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="matOgDrikke">
+        <InventoryTable tab="matOgDrikke"/>
+      </TabsContent>
+
+      <TabsContent value="varmeOgLys">
+        <InventoryTable tab="varmeOgLys"/>
+      </TabsContent>
+
+      <TabsContent value="informasjon">
+        <InventoryTable tab="informasjon"/>
+      </TabsContent>
+
+      <TabsContent value="legemidOgHygiene">
+        <InventoryTable tab="legemidOgHygiene"/>
+      </TabsContent>
+    </Tabs>
   </div>
 </template>
 
