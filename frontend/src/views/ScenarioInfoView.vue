8a4dd18c52cc92d0c8e55f34b1128c83be87bb3c
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-04-25 12:02:54 +0200

    Fixed ScenarioInfoView.vue to have the correct labels

--- frontend/src/views/ScenarioInfoView.vue
+++ frontend/src/views/ScenarioInfoView.vue
@@ -1,9 +1,67 @@
 <script setup>
 
+import { BentoCard, BentoGrid } from '@/components/ui/bento/index.js'
+
 </script>
 
 <template>
+  <div class="m-auto">
+  <BentoGrid>
+    <BentoCard
+      name="Brann"
+      description="Lær hvordan du forbereder deg på og håndterer brann i hjemmet eller nærområdet."
+      href="/scenario"
+      cta="Les mer"
+      :Icon="ArrowRightIcon"
+      customClass="col-span-2"
+    />
 
+    <BentoCard
+      name="Flom"
+      description="Få oversikt over hva du bør gjøre før, under og etter en flomsituasjon for å holde deg trygg."
+      href="/info"
+      cta="Les mer"
+      :Icon="ArrowRightIcon"
+      customClass="col-span-2"
+    />
+
+    <BentoCard
+      name="Pandemi"
+      description="Informasjon om hygiene, isolasjon og tiltak du bør følge under en smittsom sykdomsutbrudd."
+      href="/info"
+      cta="Les mer"
+      :Icon="ArrowRightIcon"
+      customClass="col-span-2"
+    />
+
+    <BentoCard
+      name="Krig"
+      description="Forstå hvordan du kan forberede deg mentalt og praktisk på uro, trusler og væpnet konflikt."
+      href="/info"
+      cta="Les mer"
+      :Icon="ArrowRightIcon"
+      customClass="col-span-2"
+    />
+
+    <BentoCard
+      name="Strømbrudd"
+      description="Slik klarer du deg uten strøm i korte og lengre perioder, og hva du bør ha tilgjengelig."
+      href="/info"
+      cta="Les mer"
+      :Icon="ArrowRightIcon"
+      customClass="col-span-2"
+    />
+
+    <BentoCard
+      name="Matmangel"
+      description="Lær hvordan du kan sikre matforsyning og ernæring i situasjoner med knapphet på mat."
+      href="/info"
+      cta="Les mer"
+      :Icon="ArrowRightIcon"
+      customClass="col-span-2"
+    />
+  </BentoGrid>
+  </div>
 </template>
 
 <style scoped>
