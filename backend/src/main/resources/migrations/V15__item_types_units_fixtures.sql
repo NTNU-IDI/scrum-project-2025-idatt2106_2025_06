41b6748abab8f38f926783e4bbe60396faee8368
jonasmbjo <jonas@bjornbom.no>
2025-04-28 14:34:39 +0200

    added fxtures for itemtype and unit

---
+++ backend/src/main/resources/migrations/V15__item_types_units_fixtures.sql
@@ -0,0 +1,14 @@
+-- Insert predefined item types
+INSERT INTO item_types (type_name) VALUES
+                                  ('food'),
+                                  ('heat_light'),
+                                  ('information'),
+                                  ('hygiene_medicine'),
+                                  ('other');
+
+-- Insert predefined item units
+INSERT INTO item_units (unit_name) VALUES
+                                  ('pieces'),
+                                  ('grams'),
+                                  ('liters'),
+                                  ('none');
