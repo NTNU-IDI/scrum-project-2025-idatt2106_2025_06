a226af62f648b06c3e895c5241bded9a23ff5861
jonasmbjo <jonas@bjornbom.no>
2025-04-28 14:35:12 +0200

    creation, deletion, and listing og items in a storage

--- backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/DTOs/ItemInstancesDTOs.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/DTOs/ItemInstancesDTOs.kt
@@ -1,14 +1,23 @@
 package edu.ntnu.idatt2106.gr6.backend.DTOs
 
+import java.math.BigDecimal
 import java.time.Instant
 import java.time.LocalDate
+import java.util.UUID
 
-data class ItemInstancesResponse(
-    val id: String,
-    val itemId: String,
+data class CreateItemInstanceRequest (
+    val name: String,
+    val description: String?,
+    val typeId: Int,
+    val unitId: Int,
     val storageId: String,
-    val expiryDate: LocalDate?,
-    val amount: Double,
-    val createdAt: Instant,
-    val updatedAt: Instant
+    val amount: BigDecimal,
+    val expiryDate: LocalDate?
+)
+
+data class StorageItemResponse(
+    val name: String,
+    val amount: BigDecimal,
+    val unit: String,
+    val expiryDate: LocalDate?
 )
--- backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/model/Item.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/model/Item.kt
@@ -1,4 +1,12 @@
 package edu.ntnu.idatt2106.gr6.backend.model
 
-class Item {
-}
+import java.time.Instant
+
+data class Item(
+    val id: String,
+    val name: String,
+    val typeId: Int,
+    val unitId: Int,
+    val createdAt: Instant,
+    val updatedAt: Instant
+)
