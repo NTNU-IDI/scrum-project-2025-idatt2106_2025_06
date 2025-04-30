48e3d66d9aaf30b2bcdb913d048f509e5d51f1d9
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-04-30 10:33:49 +0200

    Added popovers for admin settings

---
+++ frontend/src/components/DeleteModerator.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import {Button} from "@/components/ui/button/index.js";
+import {Input} from "@/components/ui/input/index.js";
+import {DialogClose} from "@/components/ui/dialog/index.js";
+
+</script>
+
+<template>
+  <Dialog>
+    <DialogTrigger>
+      <Button class="w-full bg-black text-white">
+        Fjern moderator
+      </Button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Fjern moderator</DialogTitle>
+        <Input
+          placeholder="Navn"
+          type="text"
+        />
+        <Input
+          placeholder="E-post"
+          type="text"
+        />
+      </DialogHeader>
+
+      <DialogFooter class="flex flex-col items-center">
+        <DialogClose>
+          <Button variant="outline" class="w-48">
+            Avbryt
+          </Button>
+        </DialogClose>
+        <Button class="w-48" variant="default">
+          Fjern
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
