492b5c384dd5845231579d65f48f2df83efd035e
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-04-30 12:57:55 +0200

    Added PublishAlert.vue for publishing new alerts

---
+++ frontend/src/components/PublishAlert.vue
@@ -0,0 +1,63 @@
+<script setup>
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import {Button} from "@/components/ui/button/index.js";
+import {Input} from "@/components/ui/input/index.js";
+import {Label} from "@/components/ui/label/index.js";
+import {DialogClose} from "@/components/ui/dialog/index.js";
+
+</script>
+
+<template>
+  <Dialog>
+    <DialogTrigger>
+      <Button class="w-full bg-black text-white">
+        Legg til en varsling
+      </Button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Publiser varsling</DialogTitle>
+
+        <div class="flex items-center">
+          <Label class="m-2 w-24" for="title">Navn:</Label>
+          <Input class="border w-full" id="title" placeholder="Navn på varsel" />
+        </div>
+
+        <div class="flex items-center">
+          <Label class="m-2 w-24" for="text">Varsel:</Label>
+          <Input class="border w-full" id="text" placeholder="Innhold for varsel" />
+        </div>
+
+        <div class="flex items-center">
+          <Label class="m-2 w-24" for="type">Type:</Label>
+          <Input class="border w-full" id="type" placeholder="Type varsel" />
+        </div>
+
+        <div class="flex items-center">
+          <Label class="m-2 w-24" for="time">Tidspunkt:</Label>
+          <Input class="border w-full" id="time" placeholder="Klokkeslett/dato" />
+        </div>
+      </DialogHeader>
+
+
+      <DialogFooter class="flex gap-2">
+        <DialogClose>
+          <Button variant="outline" class="flex-1">
+            Avbryt
+          </Button>
+        </DialogClose>
+        <Button class="flex-1" variant="default">
+          Publiser
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
