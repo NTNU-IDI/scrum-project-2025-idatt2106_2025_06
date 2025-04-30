73234281265c7b1f980fafa7e2170bc7d6937d7a
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-04-30 12:57:31 +0200

    Added NavBar for Admin, and a box for editing scenario-info articles

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -3,36 +3,25 @@ import { Button } from '@/components/ui/button/index.js'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card/index.js'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert/index.js'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs/index.js'
-
-import { ref } from 'vue'
-import AddModerator from '@/components/AddModerator.vue'
-import DeleteModerator from '@/components/DeleteModerator.vue'
-import ResetPasswordLink from '@/components/ResetPasswordLink.vue'
-
-const isAdmin = ref(true) // endre til false for moderator
+import PublishPost from '@/components/PublishPost.vue'
+import PublishAlert from '@/components/PublishAlert.vue'
 </script>
 
 <template>
   <div class="m-auto flex flex-wrap relative w-full py-10 gap-6 items-stretch mt-10">
-    <!-- Vis dette kortet KUN hvis man er admin -->
-    <Card v-if="isAdmin" class="flex-1 basis-1/4 min-w-[200px] gap-6 max-h-[650px]">
+    <!-- Redigere og legge til nye infosider om forskjellige scenarioer -->
+    <Card class="flex-1 basis-1/4 min-w-[200px] gap-6 max-h-[650px]">
       <CardHeader>
-        <CardTitle class="text-2xl">Rediger moderatorer</CardTitle>
+        <CardTitle class="text-2xl">Rediger scenario informasjon</CardTitle>
       </CardHeader>
       <p></p>
       <CardContent class="flex flex-col gap-2 h-full max-h-[calc(100%-80px)]">
-      <AddModerator />
-        <DeleteModerator />
-        <ResetPasswordLink />
-        <div class="grow"></div> <!-- Dette er helt forferdelig hardkoda, men la gå for nå -->
-        <Button class="w-full" variant="destructive">Logg ut</Button>
+        TBA
       </CardContent>
     </Card>
 
-    <!-- De to Card under vises alltid -->
-
     <!-- Redigere innlegg og varslinger -->
-    <Card :class="isAdmin ? 'max-h-[650px] flex-1 basis-1/4 min-w-[200px]' : 'max-h-[650px] flex-1 basis-2/5 min-w-[300px]'">
+    <Card class="'max-h-[650px] flex-1 basis-1/4 min-w-[300px]'">
       <Tabs default-value="post" class="">
         <TabsList class="grid w-full grid-cols-2">
           <TabsTrigger value="post">
@@ -46,7 +35,7 @@ const isAdmin = ref(true) // endre til false for moderator
         <!-- Varslinger-tab -->
         <TabsContent value="post">
           <div class="flex flex-col gap-2 p-5">
-          <Button >Legg til nytt innlegg</Button>
+          <PublishPost/>
             <CardContent class="flex flex-col overflow-y-auto max-h-[500px] gap-2 p-0">
               <!-- TODO Her hentes det egentlig fra databasen, dette er bare for å vise utseende og at man kan scrolle -->
               <Alert variant="default"><AlertTitle>Info: </AlertTitle><AlertDescription>En bombe er sluppet på sluppen, alle eksamener avlyst.</AlertDescription></Alert>
@@ -66,7 +55,7 @@ const isAdmin = ref(true) // endre til false for moderator
         <!-- Innlegg-tab -->
         <TabsContent value="alert">
           <div class="flex flex-col gap-2 p-5">
-            <Button >Legg til ny varsling</Button>
+            <PublishAlert/>
             <CardContent class="flex flex-col overflow-y-auto max-h-[500px] gap-2 p-0">
               <!-- TODO Her hentes det egentlig fra databasen, dette er bare for å vise utseende og at man kan scrolle -->
               <Alert variant="default"><AlertTitle>Varsel: </AlertTitle><AlertDescription>En bombe er sluppet på sluppen, alle eksamener avlyst.</AlertDescription></Alert>
@@ -87,11 +76,11 @@ const isAdmin = ref(true) // endre til false for moderator
     </Card>
 
     <!-- Redigere kart -->
-    <Card :class="isAdmin ? 'max-h-[650px] flex-1 basis-1/4 min-w-[200px]' : 'max-h-[650px] flex-1 basis-2/5 min-w-[300px]'">
+    <Card class="'max-h-[650px] flex-1 basis-1/4 min-w-[300px]'">
       <CardHeader class="grid grid-cols-2 items-center w-full">
-        <CardTitle class="text-2xl">Kart og hendelser</CardTitle>
-        <router-link class="w-auto justify-self-end" to="/admin/map">
-        <Button >Rediger</Button>
+        <CardTitle class="text-2xl whitespace-nowrap">Kart og hendelser</CardTitle>
+        <router-link class=" justify-self-end" to="/admin/map">
+        <Button>Rediger</Button>
         </router-link>
       </CardHeader>
       <CardContent class="h-[80%]">
