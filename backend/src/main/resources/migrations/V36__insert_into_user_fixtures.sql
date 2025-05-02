b8e559ea9604fd728102657860a3af30b82a8da3
mathole01 <mathias@Mathias-sin-MacBook-Pro.local>
2025-05-02 21:12:36 +0200

    Add update schema to user table

---
+++ backend/src/main/resources/migrations/V36__insert_into_user_fixtures.sql
@@ -0,0 +1,14 @@
+UPDATE users
+SET location = 'J+kzMTx8FNCXafF/epfe8GdLH9Hz5HnAqZ/EcNTTbvn2hzhSMjoEJ3aUXoTKWW8O',
+    tracking_enabled = TRUE
+WHERE id = '1e7b1d2c-3a4b-4c5d-8e6f-7a8b9c0d1e2f';
+
+UPDATE users
+SET location = 'J+kzMTx8FNCXafF/epfe8GdLH9Hz5HnAqZ/EcNTTbvn2hzhSMjoEJ3aUXoTKWW8O',
+    tracking_enabled = TRUE
+WHERE id = '2f8c2e3d-4b5c-6d7e-9f0a-1b2c3d4e5f6a';
+
+UPDATE users
+SET location = 'J+kzMTx8FNCXafF/epfe8GdLH9Hz5HnAqZ/EcNTTbvn2hzhSMjoEJ3aUXoTKWW8O',
+    tracking_enabled = TRUE
+WHERE id = '3e7aaed0-48dc-4ba1-b77a-944ec0540f94';
