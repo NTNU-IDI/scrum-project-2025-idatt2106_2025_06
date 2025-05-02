a417b87f28997f35488520d66d8fcc63ada47ac2
mathole01 <mathias@Mathias-sin-MacBook-Pro.local>
2025-05-02 21:16:51 +0200

    Add Location Service class

---
+++ backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/service/LocationService.kt
@@ -0,0 +1,38 @@
+package edu.ntnu.idatt2106.gr6.backend.service
+
+import edu.ntnu.idatt2106.gr6.backend.repository.UserLocationRepository
+import edu.ntnu.idatt2106.gr6.backend.DTOs.UserDTOs.UpdateUserLocationRequest
+import edu.ntnu.idatt2106.gr6.backend.DTOs.UserDTOs.GetUserLocationRequest
+import edu.ntnu.idatt2106.gr6.backend.model.Location
+import edu.ntnu.idatt2106.gr6.backend.util.LocationParser
+import org.springframework.stereotype.Service
+import java.sql.ResultSet
+
+@Service
+class LocationService(
+    private val locationEncryptionService: LocationEncryptionService,
+    private val userLocationRepository: UserLocationRepository,
+    private val userContextService: UserContextService,
+    private val locationParser: LocationParser
+) {
+    fun updateUserLocation(updateUserLocationRequest: UpdateUserLocationRequest) {
+        val userId: String = userContextService.getCurrentUserId().toString()
+        val encryptedLocation = locationEncryptionService.encryptLocation(
+            "POINT(${updateUserLocationRequest.location.latitude} ${updateUserLocationRequest.location.longitude})"
+        )
+        userLocationRepository.saveUserLocation(userId, encryptedLocation)
+    }
+
+    fun getUserLocation(getUserLocationRequest: GetUserLocationRequest): Location {
+        val userId: String = userContextService.getCurrentUserId().toString()
+        if(userId != getUserLocationRequest.userId) {
+            throw IllegalArgumentException("User ID does not match the current user.")
+        }
+        val encryptedLocation = userLocationRepository.findUserLocation(userId) ?:
+            throw IllegalArgumentException("User location has not been enabled")
+
+        val decryptedLocation = locationEncryptionService.decryptLocation(encryptedLocation)
+        val parsedLocation: Location = locationParser.parseLocation(decryptedLocation)
+        return parsedLocation
+    }
+}
