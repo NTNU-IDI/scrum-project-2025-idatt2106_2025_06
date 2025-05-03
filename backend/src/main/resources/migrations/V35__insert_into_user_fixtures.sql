873d6fede56a216e1d629e62692ac7c1b86fc6f9
mathole01 <mathias@Mathias-sin-MBP.lan>
2025-05-03 16:09:27 +0200

    Fix wrong version number

--- backend/src/main/resources/migrations/V35__insert_into_user_fixtures.sql
+++ backend/src/main/resources/migrations/V35__insert_into_user_fixtures.sql
@@ -1,14 +1,14 @@
 UPDATE users
-SET location = 'J+kzMTx8FNCXafF/epfe8GdLH9Hz5HnAqZ/EcNTTbvn2hzhSMjoEJ3aUXoTKWW8O',
+SET location = 'HJZJOP5G2WVeOQrUKFg9Zzo4LN6jHgBavYVY8mhxkLk=',
     tracking_enabled = TRUE
 WHERE id = '1e7b1d2c-3a4b-4c5d-8e6f-7a8b9c0d1e2f';
 
 UPDATE users
-SET location = 'J+kzMTx8FNCXafF/epfe8GdLH9Hz5HnAqZ/EcNTTbvn2hzhSMjoEJ3aUXoTKWW8O',
+SET location = 'HJZJOP5G2WVeOQrUKFg9Zzo4LN6jHgBavYVY8mhxkLk=',
     tracking_enabled = TRUE
 WHERE id = '2f8c2e3d-4b5c-6d7e-9f0a-1b2c3d4e5f6a';
 
 UPDATE users
-SET location = 'J+kzMTx8FNCXafF/epfe8GdLH9Hz5HnAqZ/EcNTTbvn2hzhSMjoEJ3aUXoTKWW8O',
+SET location = 'HJZJOP5G2WVeOQrUKFg9Zzo4LN6jHgBavYVY8mhxkLk=',
     tracking_enabled = TRUE
 WHERE id = '3e7aaed0-48dc-4ba1-b77a-944ec0540f94';
