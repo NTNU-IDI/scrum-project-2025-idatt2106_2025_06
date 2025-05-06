ad84a0ce2ad9b1a33d34af1f4d80fc41aa37f953
mathole01 <mathias@dhcp-10-22-17-201.wlan.ntnu.no>
2025-05-06 09:22:55 +0200

    Add websocket configure file

---
+++ backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/config/WebSocketConfig.kt
@@ -0,0 +1,82 @@
+package edu.ntnu.idatt2106.gr6.backend.config
+
+import edu.ntnu.idatt2106.gr6.backend.service.JwtService
+import edu.ntnu.idatt2106.gr6.backend.service.UserDetailsServiceImpl
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.config.ChannelRegistration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.messaging.simp.stomp.StompCommand
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.support.ChannelInterceptor
+import org.springframework.messaging.support.MessageHeaderAccessor
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.messaging.Message
+
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+class WebsocketConfig(
+    private val jwtService: JwtService,
+    private val userDetailsService: UserDetailsServiceImpl,
+    private val jwtHandshakeInterceptor: JwtHandshakeInterceptor,
+) : WebSocketMessageBrokerConfigurer {
+    private val logger = org.slf4j.LoggerFactory.getLogger(WebsocketConfig::class.java)
+
+    init {
+        logger.info("WebSocketConfig initialized")
+    }
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
+            .setInterceptors(jwtHandshakeInterceptor)
+        registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .addInterceptors(jwtHandshakeInterceptor)
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/topic")
+        registry.setApplicationDestinationPrefixes("/app")
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        logger.info("Configuring client inbound channel")
+        registration.interceptors(object : ChannelInterceptor {
+            override fun preSend(message: Message<*>, channel: MessageChannel): Message<*>? {
+                val accessor: StompHeaderAccessor =
+                    MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor::class.java) ?: return message
+                logger.info("Headers: {}", accessor)
+
+                logger.info("Message command: ${accessor?.command}")
+
+
+                if (StompCommand.CONNECT == accessor?.getCommand()) {
+                    val authorizationHeader: String? = accessor.getFirstNativeHeader("Authorization")
+                    val token = authorizationHeader!!.substring(7)
+
+                    logger.info("Extracted token websock: $token")
+
+                    val username: String = jwtService.extractEmailFromToken(token).toString()
+                    val userDetails = userDetailsService!!.loadUserByUsername(username)
+                    val usernamePasswordAuthenticationToken =
+                        UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities())
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken)
+
+                    accessor.setUser(usernamePasswordAuthenticationToken)
+                }
+
+                return message
+            }
+        })
+    }
+}
