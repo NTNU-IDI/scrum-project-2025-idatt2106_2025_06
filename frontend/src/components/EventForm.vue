60789b5e2de485a24b9bb15612ca751439cf57e9
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-05-07 08:51:02 +0200

    Refactored EventForm

--- frontend/src/components/EventForm.vue
+++ frontend/src/components/EventForm.vue
@@ -1,6 +1,5 @@
 <script setup>
-import {ref, defineProps, onMounted} from 'vue'
-import { watch } from 'vue'
+import { ref, defineProps, watch } from 'vue'
 import { useEventStore } from '@/stores/event.js'
 import { Card} from '@/components/ui/card/index.js';
 import {Button} from '@/components/ui/button';
@@ -16,15 +15,6 @@ import {
 
 const eventStore = useEventStore()
 
-onMounted(() => {
-  const currentDate = new Date();
-  const formattedDate = currentDate.toISOString().split('T')[0]; // Formater datoen som YYYY-MM-DD
-  const formattedTime = currentDate.toTimeString().split(' ')[0].slice(0, 5); // Formater tiden som HH:MM
-
-  date.value = formattedDate;
-  time.value = formattedTime;
-});
-
 const props = defineProps({
   token: {
     type: String,
@@ -34,32 +24,36 @@ const props = defineProps({
     type: String,
     default: 'new', //new or edit
   },
-  eventId: [String, Number],
+  eventData: Object,
 });
 
-const id = ref(null);
-const title = ref('');
-const content = ref('');
-const position = ref('');
-const radius = ref(null);
-const eventType = ref('other');
+const mode = ref(props.mode);
+const id = ref(null)
+const title = ref('')
+const description = ref('')
+const content = ref('')
+const latitude = ref('')
+const longitude = ref('')
+const radius = ref(null)
+const eventType = ref('other')
 const eventStatus = ref('ongoing')
-const description = ref('');
-const selectedSeverity = ref('info');
-const date = ref(null);
-const time = ref(null);
+const selectedSeverity = ref('low')
+const date = ref()
+const time = ref()
 
 const eventFormRefresh = () => {
-  title.value = '';
-  content.value = '';
-  description.value = '';
-  position.value = '';
-  radius.value = null;
-  eventType.value = 'other';
-  eventStatus.value = 'ongoing';
-  selectedSeverity.value = 'info'
-  date.value = null;
-  time.value = null;
+  id.value = ''
+  title.value = ''
+  content.value = ''
+  description.value = ''
+  latitude.value = ''
+  longitude.value = ''
+  radius.value = null
+  eventType.value = 'other'
+  eventStatus.value = 'ongoing'
+  selectedSeverity.value = 'low'
+  time.value = ''
+  date.value = ''
 };
 
 defineExpose({
@@ -67,73 +61,91 @@ defineExpose({
 });
 
 async function handleSubmit() {
-  console.log('Handling submit for mode:', props.mode);
+  console.log('Handling submit for mode:', props.mode)
+
+  if (!title.value || !description.value || !selectedSeverity.value || !eventType.value) {
+    alert('Fyll ut alle obligatoriske felter: Tittel, Beskrivelse, Beredskapsnivå og Hendelse.');
+    return;
+  }
+
+  let datetime = date.value && time.value ? `${date.value}T${time.value}` : null
+  console.log('sjekk', datetime)
 
   const eventPayload = {
     name: title.value,
-    content: content.value,
     description: description.value,
+    content: content.value,
     status: eventStatus.value,
-    position: position.value,
+    location: {
+      latitude: latitude.value,
+      longitude: longitude.value
+    },
+
     impact_area_radius_km: radius.value,
     type: eventType.value,
     severity: selectedSeverity.value,
-    startDate: date.value && time.value ? date.value + 'T' + time.value : null,
+    startDate: datetime ? new Date(datetime).toISOString() : null,
   }
+  console.log("Event Payload:", eventPayload);
 
   try {
-    const token = props.token;
+    const token = props.token
     if (props.mode === 'new') {
-      await eventStore.createNewEvent(eventPayload, token);
+      console.log('EventCard props:', props.eventData);
+      await eventStore.createNewEvent(eventPayload, token)
     } else if (props.mode === 'edit') {
-      const eventId = id.value;
-      await eventStore.updateExistingEvent(eventId, eventPayload, token);
+      const eventId = id.value
+
+      console.log('Updating event id:', id.value)
+      await eventStore.updateExistingEvent(eventId, eventPayload, token)
     }
   } catch (error) {
-    console.error("Feil ved innsending av hendelse:", error);
+    console.error("Feil ved innsending av hendelse:", error)
   }
 }
 
-watch(() => props.eventId, async (newId) => {
-  if (newId) {
-    console.log('Fyller skjema for eventId:', newId);
-    console.log('Fyller skjema med token:', props.token);
-    eventFormRefresh()
+//If an EventCard button is pressed
+watch(() => props.eventData, async (newData) => {
+  if (newData) {
+    mode.value = 'edit'
+    console.log('Fyller skjema for:', props.mode)
 
-    id.value = newId;
+    id.value = newData.eventId
+    title.value = newData.name
+    description.value = newData.description
+    content.value = newData.content
+    eventType.value = newData.type
+    eventStatus.value = newData.status
+    selectedSeverity.value = newData.severity
+    latitude.value = newData.location.latitude
+    longitude.value = newData.location.longitude
 
-    const eventData = await eventStore.getEventById(newId, props.token);
-    if (eventData) {
-      title.value = eventData.name;
-      description.value = eventData.content;
-      console.log(eventData.name);
-      console.log(eventData.content);
-      /**position.value = eventData.position;
-      radius.value = eventData.impact_area_radius_km;
-      eventType.value = eventData.type;
-      description.value = eventData.description;
-      selectedSeverity.value = eventData.severity;
-      if (eventData.start_date) {
-        const [datePart, timePart] = eventData.start_date.split('T');
-        date.value = datePart;
-        time.value = timePart;
-      }*/
+    if (newData.startDate) {
+      console.log('ny data dato', newData.startDate)
+      const [datePart, timePart] = newData.startDate.split('T')
+      date.value = datePart
+      time.value = timePart?.slice(0,5) ?? '00:00'
     }
   }
 });
 </script>
 
-
 <template>
   <Card class="flex flex-col gap-2 p-5">
     <div class="flex items-center">
       <Label class="m-2" for="title">Tittel</Label>
-      <Input class="border w-full" id="title" placeholder="Navn på hendelse" v-model="title" />
+      <Input
+        :class="['border w-full', !title ? 'border-blue-500' : '']"
+        id="title"
+        placeholder="Navn på hendelse"
+        v-model="title"
+      />
+
     </div>
 
     <div class="flex items-center">
       <Label class="m-2" for="description">Beskrivelse</Label>
-      <Input class="border w-full" id="description" placeholder="Detaljert beskrivelse av hendelsen" v-model="content" />
+      <Input class="border w-full" id="description" placeholder="Kort beskrivelse av hendelsen" v-model="description" />
     </div>
 
     <div class="flex align-middle items-center">
@@ -144,17 +156,33 @@ watch(() => props.eventId, async (newId) => {
             <SelectValue placeholder="Velg beredskapsnivå" />
           </SelectTrigger>
           <SelectContent>
-              <SelectItem value="info" severity="info">Info</SelectItem>
-              <SelectItem value="green" severity="low">Lav</SelectItem>
-              <SelectItem value="yellow" severity="medium">Middels</SelectItem>
-              <SelectItem value="red" severity="high">Høy</SelectItem>
+              <SelectItem value="low" severity="low">Lav</SelectItem>
+              <SelectItem value="medium" severity="medium">Middels</SelectItem>
+              <SelectItem value="high" severity="high">Høy</SelectItem>
           </SelectContent>
         </Select>
       </div>
     </div>
 
     <div class="flex align-middle items-center">
-      <Label class="m-2" for="event">Hendelse</Label>
+      <Label class="m-2" for="eventStatus">Status</Label>
+      <div class="flex gap-2 items-center">
+        <Select v-model="eventStatus">
+          <SelectTrigger class="w-[180px]">
+            <SelectValue placeholder="Velg type hendelse" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="planned">Planlagt</SelectItem>
+            <SelectItem value="ongoing">Pågår</SelectItem>
+            <SelectItem value="finished">Ferdig</SelectItem>
+            <SelectItem value="cancelled">Avbrutt</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+
+    <div class="flex align-middle items-center">
+      <Label class="m-2" for="eventType">Hendelse</Label>
       <div class="flex gap-2 items-center">
         <Select v-model="eventType">
           <SelectTrigger class="w-[180px]">
@@ -171,39 +199,28 @@ watch(() => props.eventId, async (newId) => {
       </div>
     </div>
 
-    <div class="flex align-middle items-center">
-      <Label class="m-2" for="event">Hendelse</Label>
-      <div class="flex gap-2 items-center">
-        <Select v-model="eventStatus">
-          <SelectTrigger class="w-[180px]">
-            <SelectValue placeholder="Velg type hendelse" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="natural_disaster">Naturkatastrofe</SelectItem>
-            <SelectItem value="nuclear_attack">Atomangrep</SelectItem>
-            <SelectItem value="terror_attack">Terrorangrep</SelectItem>
-            <SelectItem value="pandemic">Pandemi</SelectItem>
-            <SelectItem value="other">Annet</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-    </div>
-
     <div class="flex items-center">
-      <Label class="m-2" for="position">Posisjon</Label>
-      <Input class="border w-full" id="position" placeholder="Adresse eller koordinater" v-model="position" />
+      <Label class="m-2" for="latitude">Breddegrad</Label>
+      <div class="flex flex-col ">
+        <Label class="m-2" for="latitude">Latitude</Label>
+        <Input class="border w-full" id="latitude" placeholder="eks: 64.232321" v-model="latitude" />
+      </div>
+
+      <div class="flex flex-col ">
+        <Label class="m-2" for="longitude">Lengdegrad</Label>
+        <Input class="border w-full" id="longitude" placeholder="eks: 10.422132" v-model="longitude" />
+      </div>
     </div>
 
     <div class="flex items-center">
       <Label class="m-2" for="radius">Radius</Label>
       <input type="number" step="10" class="h-9 rounded-md border border-input max-w-[100px] text-right" id="radius" placeholder="1000" v-model.number="radius" />
-      <p class="ml-2">meter</p>
+      <p class="ml-2">km</p>
     </div>
 
-
     <div class="flex items-center">
-      <Label class="m-2" for="summary">Sammendrag</Label>
-      <Input class="border w-full" id="summary" placeholder="Sammendrag av hendelsen" v-model="description" />
+      <Label class="m-2" for="content">Mer info</Label>
+      <Input class="border w-full" id="content" placeholder="Lengre beskrivelse av hendelsen" v-model="content" />
     </div>
 
     <div class="flex align-middle items-center">
