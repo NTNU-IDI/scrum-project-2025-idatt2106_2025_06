b88eddb55a940a3111f70648a688a6457c024945
Carine Margrethe Rondeel <carinemr@stud.ntnu.no>
2025-05-08 13:36:49 +0200

    Change between log in or my profile

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+import { useSessionStore } from '@/stores/session.js'
+import { computed } from 'vue'
+
+const sessionStore = useSessionStore();
+
+const isLoggedIn = computed(() => sessionStore.token);
+</script>
 
 <template>
   <nav class="sticky top-0 z-50 w-full py-2 shadow">
@@ -9,15 +16,16 @@
       </router-link>
       <div class="gap-10 hidden sm:flex h-10 items-center">
         <router-link class="hover:scale-110 transition" to="/alerts">Varslinger</router-link>
-        <router-link class="hover:scale-110 transition" to="/inventory">Lager</router-link>
+        <router-link class="hover:scale-110 transition" to="/inventory">Beredskapslager</router-link>
         <router-link class="hover:scale-110 transition" to="/map">Kart</router-link>
       </div>
       <router-link
-        class="flex min-w-40 justify-end h-10 gap-4 items-center hover:scale-110 transition"
+        class="flex max-w-100 min-w-100 justify-end h-10 gap-4 items-center hover:scale-110 transition "
         to="/profile"
       >
-        <img alt="avatar" src="" />
-        <p>Bob</p>
+        <p v-if="!isLoggedIn">Logg inn</p>
+        <p v-else>Min profil</p>
+
       </router-link>
     </div>
   </nav>
