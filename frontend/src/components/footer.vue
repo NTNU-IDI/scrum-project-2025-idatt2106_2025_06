383d7459739397f72dd1095bb788ce05659ae145
henrik <henrik.utistog.hausberg@outlook.com>
2025-05-08 11:16:56 +0200

    Added a placeholder footer and changed the styling of home page. This might break the layout of some pages.

---
+++ frontend/src/components/footer.vue
@@ -0,0 +1,78 @@
+<script lang="ts" setup></script>
+
+<template>
+  <footer class="pt-10">
+    <div class="p-10 border gap-4 flex flex-col">
+      <div class="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+        <div class="col-span-full xl:col-span-2">
+          <a class="flex font-bold items-center gap-3" href="/">
+            <img alt="logo" class="h-10 w-10" src="/krisefikserLogo.svg" />
+            <h3 class="text-2xl font-bold">KRISEFIKSER</h3>
+          </a>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <h3 class="font-bold text-lg">Contact</h3>
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Github </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Twitter </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Instagram </a>
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <h3 class="font-bold text-lg">Platforms</h3>
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> iOS </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Android </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Web </a>
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <h3 class="font-bold text-lg">Help</h3>
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Contact Us </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> FAQ </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Feedback </a>
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <h3 class="font-bold text-lg">Socials</h3>
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Twitch </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Discord </a>
+          </div>
+
+          <div>
+            <a class="opacity-60 hover:opacity-100" href="#"> Dribbble </a>
+          </div>
+        </div>
+      </div>
+
+      <h3>2025 Developed by Group 6</h3>
+    </div>
+  </footer>
+</template>
