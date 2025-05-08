81294ad77b68cc88646822f63f699ddfa2397e0f
Jonasmbj <jonas@bjornbom.no>
2025-05-08 11:14:32 +0200

    fixtures for permission and roles for storage functions

---
+++ backend/src/main/resources/migrations/V46__permission_fixtures.sql
@@ -0,0 +1,7 @@
+INSERT INTO permissions (name, description) VALUES
+('JOIN_STORAGE', 'Allows a user to join a storage'),
+('GET_STORAGE_MEMBERS', 'Allows a user to retrieve all members in a storage'),
+('REMOVE_STORAGE_MEMBERS', 'Allows a user to remove users from a storage'),
+('GET_STORAGE', 'Allows a user to retrieve all their own in a storages'),
+('DELETE_STORAGE', 'Allows a user to delete their own storage')
+;
