5bcd58a5b40a36b4c709f111370ef4fee7c6542f
Sigrid Hoel <sigrho@ntnu.no>
2025-05-09 20:59:06 +0200

    removed dublicate button

---
+++ frontend/src/stores/moderator.js
@@ -0,0 +1,45 @@
+// src/stores/moderator.js
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import {
+  createModerator,
+  removeModerator,
+  fetchModerators,
+} from '@/mock/moderatorApiMock'
+
+export const useModeratorStore = defineStore('moderator', () => {
+  const moderators = ref([])
+  const moderatorEmail = ref('')
+
+  async function fetchAll(){
+    try {
+      const response = await fetchModerators()
+      moderators.value = response.map(m => ({
+        ...m,
+        isCurrentUser: m.email === moderatorEmail.value
+      }))
+    } catch (error) {
+      console.error('Error fetching moderators:', error)
+    }
+  }
+
+  async function add(name, email, username) {
+    try {
+      await createModerator(name, email, username)
+      await fetchAll()
+    } catch (error) {
+      console.error('Error adding moderator:', error)
+    }
+  }
+
+  async function remove(name, email, id, username) {
+    try {
+      await removeModerator(name, email, id, username)
+      await fetchAll()
+    } catch (error) {
+      console.error('Error removing moderator:', error)
+    }
+  }
+
+  return { moderators, fetchAll,  add, remove }
+})
