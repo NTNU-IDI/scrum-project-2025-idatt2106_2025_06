b81f497ad450a32bea7b800ec067ca274a42eec2
Cathrine Evensen <cathreve@stud.ntnu.no>
2025-05-09 20:10:17 +0200

    fix inventory test

--- frontend/cypress/e2e/user.cy.js
+++ frontend/cypress/e2e/user.cy.js
@@ -1,15 +1,11 @@
-// https://on.cypress.io/api
-// cypress/e2e/user.cy.ts
-
 describe('User-userstories', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/login');
-
     cy.get('#email').clear().type('pierbattista.pizzaballa@mail.com');
     cy.get('#password').clear().type('wordpass');
     cy.get('button[type="submit"]').click();
 
-    cy.intercept('POST', 'http://localhost:8080/Status', (req) => {
+    cy.intercept('POST', 'http://localhost:8080/auth/login', (req) => {
       req.reply((res) => {
         expect(res.statusCode).to.equal(200);
         expect(res.body).to.have.property('Success', true);
@@ -20,11 +16,19 @@ describe('User-userstories', () => {
   it('user should not go to admin page', () => {
     cy.visit('http://localhost:5173/admin');
     cy.url().should('not.include', '/admin');
+    cy.url().should('include', '/');
   });
 
-  it('user should not go to admin page', () => {
+  it('user should not go to admin map page', () => {
+    cy.visit('http://localhost:5173/admin/map');
+    cy.url().should('not.include', '/admin/map');
+    cy.url().should('include', '/');
+  });
+
+  it('user should not go to admin map page', () => {
+    cy.wait(2000);
+
     cy.visit('http://localhost:5173/inventory');
-    cy.url().should('include', '/inventory');
-    cy.get('newItem').click();
+
   });
 });
