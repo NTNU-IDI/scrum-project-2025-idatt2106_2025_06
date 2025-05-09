a976379a535c5770601a7f73ca0c25c19e80d241
Jonasmbj <jonas@bjornbom.no>
2025-05-09 10:40:02 +0200

    documenting checkpoint repo class

--- backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/repository/CheckpointRepository.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2106/gr6/backend/repository/CheckpointRepository.kt
@@ -4,9 +4,19 @@ import edu.ntnu.idatt2106.gr6.backend.model.Checkpoint
 import org.springframework.stereotype.Repository
 import javax.sql.DataSource
 
+/**
+ * Repository class responsible for db operations related to checkpoint management
+ *
+ * @property dataSource The data source for database connections
+ */
 @Repository
 class CheckpointRepository(private val dataSource: DataSource) {
 
+    /**
+     * Retrieves all available check points from the check point list
+     *
+     * @return List of all [Checkpoint]s
+     */
     fun getAllCheckpoints(): List<Checkpoint> {
         val sql = "SELECT id, name, description FROM checkpoint_list"
         val checkpoints = mutableListOf<Checkpoint>()
@@ -30,6 +40,12 @@ class CheckpointRepository(private val dataSource: DataSource) {
         return checkpoints
     }
 
+    /**
+     * Retrieves all checkpoints a user has already checked
+     *
+     * @param userId ID of the user that is linked to the checkpoint
+     * @return List of [Checkpoint]s linked to the user
+     */
     fun getCheckpointsByUserId(userId: String): List<Checkpoint> {
         val sql = """
         SELECT c.id, c.name, c.description
@@ -61,6 +77,13 @@ class CheckpointRepository(private val dataSource: DataSource) {
     }
 
 
+    /**
+     * Updated the checkpoints linked to a user
+     *
+     * @param userId ID of the user which linked check points is to be updated
+     * @param entries List of [Pair]s containing an ID and checkpoint ID to assign
+     * @return True if the transaction is successful, otherwise false
+     */
     fun replaceCheckpointsForUser(userId: String, entries: List<Pair<String, String>>): Boolean {
         val deleteSql = "DELETE FROM user_checkpoint_list WHERE user_id = ?"
         val insertSql = """
@@ -100,6 +123,12 @@ class CheckpointRepository(private val dataSource: DataSource) {
         }
     }
 
+    /**
+     * Retrieves the percentage of check points linked with a user
+     *
+     * @param userId ID of the user its checking percentage on
+     * @return The percentage of checked check points
+     */
     fun getUserCheckpointCompletionPercentage(userId: String): Double {
         val sql = """
         SELECT 
